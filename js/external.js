44841416499addd8e03d58304eaa5deb13969b06
zellgatos <zellbrawl@yahoo.com>
2017-08-15 09:26:45 -0500

    completed java inline and external exercise

--- js/external.js
+++ js/external.js
@@ -7,7 +7,7 @@ alert('Welcome to my Website!');
 var userInput = prompt('What is your favorite color?');
 console.log('The user entered:' + userInput);
 
-alert(userInput + 'is my favorite color too.');
+alert('sweet,' + userInput + 'is my favorite color too.');
 
 
 
