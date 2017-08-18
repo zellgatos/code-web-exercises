0b6b4dc399d5cd720f3790232909a900f7682910
zellgatos <zellbrawl@yahoo.com>
2017-08-18 15:02:12 -0500

    first array exercise

---
+++ js/iterating.js
@@ -0,0 +1,21 @@
+(function(){
+    "use strict";
+
+    var names = ['Jon', 'Sansa', 'Aria', 'Bran'];
+
+    console.log("there are " + names.length +" names in the names array.");
+
+
+    console.log(names.length);
+
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+    console.log(names[4]);
+
+    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+
+    // TODO: Create a log statement that will log the number of elements in the names array.
+
+    // TODO: Create log statements that will print each of the names array elements individually.
+})();
