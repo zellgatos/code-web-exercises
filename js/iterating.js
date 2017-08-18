d71ab057ea537683e6186b84f9f263b36b2c0d00
zellgatos <zellbrawl@yahoo.com>
2017-08-18 16:13:15 -0500

    edit for array exercise

--- js/iterating.js
+++ js/iterating.js
@@ -5,14 +5,13 @@
 
     console.log("there are " + names.length +" names in the names array.");
 
+    for (var i = 0; i < names.length; i++) {
+        console.log( i + " is " + names[i]);
+    }
 
-    console.log(names.length);
-
-    console.log(names[0]);
-    console.log(names[1]);
-    console.log(names[2]);
-    console.log(names[3]);
-
+    names.forEach(function (element, index, array) {
+        console.log(array);
+    });
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
 
     // TODO: Create a log statement that will log the number of elements in the names array.
