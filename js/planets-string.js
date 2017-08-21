91f42bd52bae16acaa39aa24b7a2c5eddd7a193b
zellgatos <zellbrawl@yahoo.com>
2017-08-21 13:32:35 -0500

    strings and join for arrays exercise completed

---
+++ js/planets-string.js
@@ -0,0 +1,20 @@
+(function(){
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+
+    // TODO: Convert planetsString to an array, save it to planetsArray.
+    var planetsAsString = planetsString.split(",");
+    console.log(planetsAsString);
+
+    var planetsAsArray = planetsArray.join(",");
+    console.log(planetsAsArray);
+
+    // TODO: Create a string with <br> tags between each planet. console.log() your results.
+    //       Why might this be useful?
+
+    // Bonus: Create a second string that would display your planets in an undordered list.
+    //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
+    //        console.log() your results.
+})();
