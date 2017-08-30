d6a5fba03643fd8d52d62b5243b00912d9913386
zellgatos <zellbrawl@yahoo.com>
2017-08-30 10:33:26 -0500

    strings and join for arrays exercise completed

---
+++ js/google_maps_api.js
@@ -0,0 +1,30 @@
+(function(){
+    "use strict";
+    var address = "codeup";
+    var codeup = { lat: 29.426791, lng: -98.489602 };
+    var mapOptions = {
+      zoom: 15,
+      mapTypeId: "roadmap",
+      center: codeup
+    };
+    var geocoder = new google.maps.Geocoder();
+               geocoder.geocode(
+               { "address": address }, function(results, status) {
+
+               if (status == google.maps.GeocoderStatus.OK) {
+
+                   console.log(results);
+
+                   map.setCenter(results[0].geometry.location);
+               } else {
+
+                   alert("Geocoding was not successful - STATUS: " + status);
+               }
+           });
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var marker = new google.maps.Marker({
+        position: codeup,
+        map: map
+    })
+})();
