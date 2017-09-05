b6de5000c7565e4bb07ed60bd54bd3fb018faa2d
zellgatos <zellbrawl@yahoo.com>
2017-09-05 14:29:52 -0500

    jquery exercise essentials traversing

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -4,4 +4,39 @@
         $("dd").toggleClass("invisible");
     });
 
+    $("dt").click(function(){
+        $(this).css("background-color", "yellow");
+    });
+
+    $("#highlight").on("click", highlightAllLastListItems);
+    function highlightAllLastListItems () {
+        $('ul').each(highlightLastListItem);
+    }
+
+    function highlightLastListItem () {
+        $(this)
+            .children()
+            .last()
+            .css("background-color", "yellow")
+        ;
+    }
+
+    $("h3").on("click", makeAllListItemsBold);
+    function makeAllListItemsBold() {
+        $(this)
+            .next()
+            .children()
+            .css("font-weight", "bold")
+        ;
+    }
+
+    $("li").on("click", makeFirstSiblingBlue);
+    function makeFirstSiblingBlue() {
+        $(this)
+            .parent()
+            .children()
+            .first()
+            .css("color", "blue")
+        ;
+    }
 })();
